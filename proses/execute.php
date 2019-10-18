5cdf9896e39a80cc16cc27b3bfd97b4e5fb66182
CodeOfKaizer <raditya2678@gmail.com>
2019-10-18 14:53:38 +0800

    ok

--- proses/execute.php
+++ proses/execute.php
@@ -3,7 +3,11 @@ require_once 'db/initial.php';
   if (isset($_POST['add_anggota'])) {
     
     $values = [
-        
+        $nama = $_POST['nama'],
+        $alamat = $_POST['alamat'],
+        $tgl_lahir = $_POST['tgl_lahir'],
+        $tmpt_lahir = $_POST['tmpt_lahir'],
+        $telp = $_POST['no_telp']
     ];
 
     $add = $addAnggota->add($values);
